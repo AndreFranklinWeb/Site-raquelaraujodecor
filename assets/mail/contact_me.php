710a846e193f2c90668924fb1cbc9a6b817a519c
Andre <andrefranklinweb@gmail.com>
2020-07-20 21:17:20 -0300

    Criado os icones das redes sociais

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Crie o email e envie a mensagem
-$to = "andrefranklinweb@gmail.com"; // Adicione seu endereço de e-mail entre "", substituindo seunome@seudominio.com.br - É para onde o formulário enviará uma mensagem.
+$to = "andrefranklincel@gmail.com"; // Adicione seu endereço de e-mail entre "", substituindo seunome@seudominio.com.br - É para onde o formulário enviará uma mensagem.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "Você recebeu uma nova mensagem do formulário de contato do site.\n\n"."Aqui estão os detalhes:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: andrefranklinweb@gmail.com\n"; // Este é o endereço de email a partir do qual a mensagem gerada será. Recomendamos o uso de algo como noreply@yourdomain.com.
+$headers = "From: andrefranklincel@gmail.com\n"; // Este é o endereço de email a partir do qual a mensagem gerada será. Recomendamos o uso de algo como noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
